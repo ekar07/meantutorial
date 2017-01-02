cd47a3b132ceea56dfd5aa702bbb5ecc57261fc5
ed <edkarwacki@gmail.com>
2017-01-02 17:17:24 -0600

    Designing the UI done

--- server.js
+++ server.js
@@ -33,12 +33,7 @@ db.once('open', function callback() {
 	console.log('meantutorial db opened');
 });
 
-var messageSchema = mongoose.Schema({message: String});
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function(err, messageDoc) {
-	mongoMessage = messageDoc.message;
-});
+
 
 
 app.get('/partials/:partialPath', function(req, res){
@@ -47,9 +42,7 @@ app.get('/partials/:partialPath', function(req, res){
 
 
 app.get('*', function(req, res){
-	res.render('index', {
-		mongoMessage: mongoMessage
-	});
+	res.render('index');
 });
 
 var port = 3030;
